15a4fd96d0e1f7abf07812de5e8b248ec9ce5707
Kush Daga <kushdaga@hotmail.com>
2020-09-28 23:23:50 +0530

    added github

--- src/components/app.js
+++ src/components/app.js
@@ -5,13 +5,36 @@ import Header from "./header";
 
 // Code-splitting is automated for `routes` directory
 import Home from "../routes/home";
-
+import imageGithub from "../assets/GitHub-Mark-64px.png";
 const App = () => (
   <div id="app">
     <Header />
     <Router>
       <Home path="/" />
     </Router>
+    <a
+      href="https://github.com/kush-daga/altify"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <div
+        style={{
+          position: "fixed",
+          display: "flex",
+          bottom: "20px",
+          right: 0,
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "20px",
+          cursor: "pointer",
+          backgroundColor: "#f3f3f3",
+          padding: "16px",
+          borderRadius: "5px",
+        }}
+      >
+        <img src={imageGithub} width={32} alt="github" />
+      </div>
+    </a>
   </div>
 );
 
